f71595f61b05441e02940d657f912a0a0e0b628e
Mayur Kale <kalemayur8@gmail.com>
2019-08-28 16:22:57 +0400

    Add files via upload

---
+++ src/main/java/com/test/ssl/TestSSLApplication.java
@@ -0,0 +1,13 @@
+package com.test.ssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSSLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSSLApplication.class, args);
+	}
+
+}
